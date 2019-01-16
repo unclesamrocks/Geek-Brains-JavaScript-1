206064c89609f6d3539fa7f777cf9d16facb1b37
Nikita Moiseev <moiseev.nikita@gmail.com>
2019-01-16 14:14:47 +0300

    Lessons 01, 02, 03

---
+++ Lesson 01/js#1 HW#1.js
@@ -0,0 +1,10 @@
+let celcius = 20;
+let fahrenheit = (9 / 5) * celcius + 32;
+alert(fahrenheit);
+//
+let name = 'Vasily';
+let admin = name;
+alert(admin);
+//
+let g = 1000 + "108"; // будет = '1000108'
+alert(g);
